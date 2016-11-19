668a54694abfb291e0c1cc63e5ac9fb21a868303
watch1k <host.sf@gmail.com>
2016-11-19 03:24:35 +0200

    update

--- build/js/lib/common.js
+++ build/js/lib/common.js
@@ -3,6 +3,106 @@ $(document).ready(function () {
 
 	//for IE9
 	svg4everybody();
+	initValidator();
+	initPhoneMask();
+
+	google.maps.event.addDomListener(window, 'load', initMap());
+
+	function initMap() {
+		var myLatLng = {lat: 34.0627069, lng: -118.3539114};
+		var mapProp = {
+			center: myLatLng,
+			zoom: 12,
+			scrollwheel: false,
+			draggable: true,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+		var map = new google.maps.Map(document.getElementById("map"), mapProp);
+		var marker = new google.maps.Marker({
+			position: myLatLng,
+			map: map,
+			title: 'Perfect Wedding'
+		});
+	}
+
+	(function () {
+		var form = $('.js-form'),
+			initModalBtn = $('.js-init-modal');
+
+		form.on('submit', function () {
+			var href = initModalBtn.data('href');
+			$.get('modals/' + href + '.html', function (data) {
+				$('body').append(data);
+				$('.js-modal').fadeIn();
+				initModalClose();
+			});
+			return false;
+		});
+	})();
+
+	function initModalClose() {
+		var modal = $('.js-modal'),
+			closeBtn = $('.js-modal-close');
+
+		closeBtn.on('click', function () {
+			$(this).closest(modal).fadeOut(function () {
+				$('body').removeClass('is-locked');
+				$(this).remove();
+				$('form').trigger('reset');
+			});
+		});
+
+		modal.on('click', function (e) {
+			if (!$(this).is(e.target)) {
+				//code here
+			} else {
+				$(this).fadeOut(function () {
+					$('body').removeClass('is-locked');
+					$(this).remove();
+				});
+			}
+		});
+	}
+
+	function initValidator(){
+		$.validate({
+			validateOnBlur : true,
+			showHelpOnFocus : false,
+			addSuggestions : false,
+			scrollToTopOnError: false,
+			borderColorOnError : '#FF0000'
+		});
+	}
+
+	function initPhoneMask(){
+		var phoneInput = $(".js-phone-mask");
+
+		phoneInput.mask("+9(999)999-99-99");
+
+		//SET CURSOR POSITION
+		$.fn.setCursorPosition = function(pos) {
+			this.each(function(index, elem) {
+				if (elem.setSelectionRange) {
+					elem.setSelectionRange(pos, pos);
+				} else if (elem.createTextRange) {
+					var range = elem.createTextRange();
+					range.collapse(true);
+					range.moveEnd('character', pos);
+					range.moveStart('character', pos);
+					range.select();
+				}
+			});
+			return this;
+		};
+
+		phoneInput.on('focus', function(){
+			var _this = $(this);
+
+			setTimeout(function() {
+				_this.setCursorPosition(1);
+			},100);
+		});
+	}
 
 	// Clear placeholder
 	(function () {
--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,6 @@
 //=include lib/svg4everybody.min.js
 //=include lib/slick.min.js
 //=include lib/ion.rangeSlider.min.js
+//=include lib/jquery.form-validator.min.js
+//=include lib/jquery.maskedinput.min.js
 //=include lib/common.js
